d1d19d310ca353a2d4271cc4b5d0ad46d2ea9a16
rusticpenguin <georgechios.music@gmail.com>
2018-08-27 14:59:45 -0600

    Basic router functionality. Can find display page titles based off of url

--- public/app.js
+++ public/app.js
@@ -1,7 +1,42 @@
-
-
 (function initializePage(){
-    if (window.location.href.includes("hello")){
-        console.log("hello World")
-    }
+    const url = "./georgejs/routes/routes.json"
+    fetch(url)
+        .then(res => res.json())
+        .then(routePage)
 })()
+
+const state = {
+    routeName: "",
+    component: "",
+    formData: {
+        
+    }
+}
+
+function routePage(data){
+    let routes = data.routes;
+    findCurrentRoute(routes)
+    renderPage();
+}
+
+function findCurrentRoute(data){
+    let routes = data;
+    for(let i = 0; i < routes.length; i++){
+        if (window.location.href.includes(routes[i].name)){
+            return((state.routeName = routes[i].name) && (state.component = routes[i].component));
+        }
+    };
+    return((state.routeName = routes[0].name) && (state.component = routes[0].component));
+}
+
+function renderPage(){
+    document.title = state.routeName;
+    gComponent = document.getElementById("gComponent");
+    gComponent.innerhtml = fetchComponent();
+}
+
+
+function logThis(data){
+    console.log(data);
+    return data
+}
